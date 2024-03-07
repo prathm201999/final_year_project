773f2a2933f482bbb3cbadca5e27b8c02d04da6e
Prathmesh Rajendra Ingole <46246793+prathm201999@users.noreply.github.com>
2024-03-07 15:48:35 +0530

    Revert "2n test"

--- js-assignment-master/src/routes/health.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-    app.route('/health').get(async (req, res, next) => {
-        try {
-            return res.json({ status: 'OK' });
-        } catch (err) {
-            return next(err);
-        }
-    });
-}
